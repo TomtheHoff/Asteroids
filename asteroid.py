f12942ff4725c2f480aaf3e665f28cfad2defe2f
TomtheHoff <hofstetterthomas@gmx.ch>
2024-10-28 18:16:11 +0100

    moddified asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,3 +1,8 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
